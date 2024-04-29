904f127e9765ba5678e3acf0ed3640bd10d46b42
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-04-30 02:36:42 +0600

    debug done

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,14 +11,16 @@ const Home = () => {
         <div className="w-11/12 mx-auto">
             <Slider></Slider>
             <Country></Country>
-            <div className=" my-20 text-center">
-                <h1 className="text-3xl font-bold">Tourists Spot</h1>
+            <div className=" my-20 ">
+                <h1 className="text-3xl font-bold text-center">Tourists Spot</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16  w-11/12 mx-auto my-10" data-aos="fade-down" data-aos-delay="50">
                     {
                         spots.map(spot => <Cards key={spot._id} spot={spot}></Cards>)
                     }
                 </div>
+                <div className="text-center">
                 <Link className="text-center" to='/allSpots'><button className="btn bg-[#23BE0A] text-white ">View All Tourists Spot</button></Link>
+                </div>
             </div>
             <Video></Video>
             <NewsLetter></NewsLetter>
